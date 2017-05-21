5bf09c6907b4f107f9480cb2aa06544a9339e4c1
Dayakar Marri <IDHAYA@dayakars-macbook-air.local>
2017-05-20 21:00:08 -0400

    Initialized the class Rating Control

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class RatingControl: UIStackView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+   // MARK Initialization
+    
+     override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
-
+            
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        fatalError("init(coder:) has not been implemented")
+        
+    }
 }
