8ac5c975bca3b1507c1505a5d9f114ae5018c14c
Dayakar Marri <IDHAYA@dayakars-macbook-air.local>
2017-05-20 21:11:56 -0400

    created setup buttons

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -15,10 +15,32 @@ class RatingControl: UIStackView {
      override init(frame: CGRect) {
         super.init(frame: frame)
     }
-            
+    
     required init(coder: NSCoder) {
         super.init(coder: coder)
         fatalError("init(coder:) has not been implemented")
         
     }
+    
+    //MARK: Private Methods
+    
+    private func setupButtons() {
+        // create button
+        
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive
+            = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive =
+        true
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
+    }
+    
+    
 }
